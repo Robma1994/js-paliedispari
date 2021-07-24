921fcea0034b9a0add225fec76b1d8de5836321d
Roberto <robertomartinonwg@gmail.com>
2021-07-24 09:08:38 +0200

    correzione esercizio pari dispari

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*Pari e Dispari
+//Pari e Dispari
 // 1 - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 var sceltaUtentePariDispari = prompt("scegli pari o dispari");
 console.log("L'utente ha scelto " + sceltaUtentePariDispari)
@@ -27,35 +27,36 @@ console.log("La somma tra i due numeri è " + somma);
 function pariDispari (x) {
     if ((x % 2 ) == 0) {
         return "pari"; 
-    }  
-    return "dispari";    
+    } 
+    return "dispari"; 
+
 }
+
 var pari_dispari = pariDispari(somma);
 console.log(pari_dispari);
 // 5 - Dichiariamo chi ha vinto.
 
-if (sceltaUtentePariDispari == somma) {
+if (sceltaUtentePariDispari == pari_dispari) {
     console.log("L'utente ha vinto");
-} else {
+} 
+else{
     console.log("L'utente ha perso");
 }
-*/
 
+/*
 //Palidroma
 // 1 - Chiedere all’utente di inserire una parola
 
+
+var nome = prompt("inserisci un nome e non una frase");
+nome = nome.toString().toUpperCase();
 // 2 - Creare una funzione per capire se la parola inserita è palindroma
-
-
-var nome = prompt("inserisci un nome");
 
 function specchio(x) {
     var inversa = ''; //valore vuoto
     //Passa in rassegna tutti i caratteri del nome aggiunto dall'utente
     for (var i = 0; i < nome.length; i++) {
-        inversa = x[i] + inversa;/*Al primo ciclo sarà - INVERSA = prima lettera + valore vuoto.
-        Inversa avrà un valore pari alla prima lettera.
-        Al secondo ciclo sarà - INVERSA= seconda lettera nome + valore inversa e così via */
+        inversa = x[i] + inversa;/
     }
     if ((nome) == inversa) {
         return "Il nome scritto è palindromo";
@@ -66,5 +67,5 @@ function specchio(x) {
 
 var confronto = specchio(nome);
 console.log(confronto);
-
+*/
 
